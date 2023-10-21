0e01fdaac45ef22b6810709420d968259ad0edb6
silvsilvsilv <johnleonilsilva24@gmail.com>
2023-10-21 23:12:27 +0800

    huffmanCoding with graph

--- griddy algorithms/huffmanCoding.py
+++ griddy algorithms/huffmanCoding.py
@@ -1,6 +1,12 @@
 # A Huffman Tree Node
 import heapq
+import networkx as nx
+from EoN import hierarchy_pos
+import matplotlib.pyplot as plt
 
+totalCodeFreq:int = 0
+
+valList =[]
 class node:
 	def __init__(self, freq, symbol, left=None, right=None):
 		# frequency of symbol
@@ -25,8 +31,10 @@ class node:
 # utility function to print huffman
 # codes for all symbols in the newly
 # created Huffman tree
-def printNodes(node, val=''):
+def printNodes(node:node, val=''):
 
+	global totalCodeFreq
+	global valList
 	# huffman code for current node
 	newVal = val + str(node.huff)
 
@@ -41,6 +49,55 @@ def printNodes(node, val=''):
 		# display its huffman code
 	if(not node.left and not node.right):
 		print(f"{node.symbol} -> {node.freq} -> {newVal}({len(newVal)}) = {len(newVal)*int(node.freq)}")
+		totalCodeFreq += (len(newVal) * int(node.freq))
+		valList.append((node.symbol,newVal))
+
+def add_edges(parent, G):
+    """Make a NetworkX graph that represents the tree."""
+    if parent is None:
+        return
+    
+    for child in (parent.left, parent.right):
+        if child:
+            G.add_edge(parent, child)
+            add_edges(child, G)
+
+def get_labels(parent, labels):
+    if parent is None:
+        return
+    
+    if parent.symbol == '\0':
+        labels[parent] = parent.freq
+    else:
+        labels[parent] = parent.symbol
+        
+    get_labels(parent.left, labels)
+    get_labels(parent.right, labels)
+
+def get_edge_labels(parent, edge_labels):
+    if parent is None:
+        return
+    
+    if parent.left:
+        edge_labels[parent, parent.left] = '0'
+        get_edge_labels(parent.left, edge_labels)
+        
+    if parent.right:
+        edge_labels[parent, parent.right] = '1'
+        get_edge_labels(parent.right, edge_labels)
+
+def draw_tree(tree):
+    G = nx.DiGraph()
+    add_edges(tree, G)
+    pos = hierarchy_pos(G)
+    labels = {}
+    get_labels(tree, labels)
+    edge_labels = {}
+    get_edge_labels(tree, edge_labels)
+    subax1 = plt.subplot()
+    nx.draw(G, pos, labels=labels, alpha=0.4)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='C1')
+    plt.show()
 
 def huffmanTree():
 	# characters for huffman tree
@@ -86,16 +143,20 @@ def huffmanTree():
 
 	# //print(charFreqDict)
 
+	test = []
 	# converting characters and frequencies
 	# into huffman tree nodes
 	for x in range(1,len(y),1):
 		
 		if x == 1:
-			heapq.heappush(nodes, node(charFreqDict.get(y[x]), y[x]))
+			heapq.heappush(nodes, node(charFreqDict.get(y[x-1]), y[x-1]))
+			heapq.heappush(test, node(charFreqDict.get(y[x-1]), y[x-1]))
 		
 		if y[x] != y[x-1]:
 			heapq.heappush(nodes, node(charFreqDict.get(y[x]), y[x]))
+			heapq.heappush(test, node(charFreqDict.get(y[x]), y[x]))
 
+	
 	while len(nodes) > 1:
 
 		# sort all the nodes in ascending order
@@ -110,13 +171,37 @@ def huffmanTree():
 		# combine the 2 smallest nodes to create
 		# new node as their parent
 		newNode = node(left.freq+right.freq, left.symbol+right.symbol, left, right)
-
+		
 		heapq.heappush(nodes, newNode)
+		heapq.heappush(test,newNode)
 	
-	print("Huffman Tree\n")
+	test.reverse()
+	for i in range(len(test)):
+		print(f"{test[i].symbol} = {test[i].huff}", end=",")
+
+	print("\nHuffman Tree\n")
 	# Huffman Tree is ready!
 
 	printNodes(nodes[0])
+
+	totalCharFreq = 0
+	for i in range(len(list(charFreqDict.values()))):
+		totalCharFreq += list(charFreqDict.values())[i]
+
+	print(f"\nTotal char frequency: {totalCharFreq}\nTotal code frequency: {totalCodeFreq}\nSaved bits: {(totalCharFreq*8) - (totalCodeFreq)}")
+
+	# ? For binary tree representation
+	# * Initialize root
+
+	tree = nodes[0]
+
+	draw_tree(tree)
+	
+	testValues = []
+	for i in range(len(test)):
+		testValues.append(test[i].symbol)
+
+	print(valList)
 
 huffmanTree()
 
--- griddy algorithms/test.py
+++ griddy algorithms/test.py
@@ -1,28 +1,120 @@
-charFrequency = 1
-y = ['a', 'a', 'a', 'b', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'd', 'e', 'e', 'e', 'e', 'e']
-charFreqDict = {}
+from collections import Counter, namedtuple
+from heapq import heapify, heappop, heappush
+import heapq
+import networkx as nx
+from EoN import hierarchy_pos
+from pyvis.network import Network
+import matplotlib.pyplot as plt
 
-# for i in range(1,len(y),1):
-# 	print(y[i])
-print(y[-1])
-for i in range(1,len(y),1):
-		
-		# sets char as key and charFrequency as value if not in dict
-		charFreqDict.setdefault(y[i],charFrequency)
+class Node:
+    def __init__(self, count, letter,left=None,right=None):
+        self.count = count
+        self.letter = letter
+        self.left = left
+        self.right = right
 
-		if y[i] == y[i-1]:
-			charFrequency += 1
-			charFreqDict[y[i]] = charFrequency 
-		elif y[i] != y[i-1]:
-			charFrequency = 1
-			charFreqDict[y[i]] = charFrequency 
+    def __lt__(self, nxt):
+            return self.count < nxt.count
 
 
-for x in range(1,len(y),1):
-	if x == 1:
-		print(charFreqDict.get(y[x]),y[x])
-		
-	if y[x] != y[x-1]:
-		print(charFreqDict.get(y[x]), y[x])
-		
-# print(charFreqDict)
+text = "bachelorofscienceincomputersciencecollegeofartsandsciences"
+c = Counter(text)
+print(c)
+
+# Node = namedtuple('Node', ['count', 'letter', 'left', 'right'])
+
+# left = Node(4, 'a', None, None)
+# print(left)
+# right = Node(2,'n',None,None)
+#print(right)
+
+# count = left.count + right.count
+root = Node(None, '\0', None, None)
+#print(root)
+
+nodes = [Node(count, letter, None, None) 
+         for (letter, count) in c.items()]
+
+# heap = nodes.copy()
+# heapify(heap)
+
+def make_tree(heap):
+    while len(heap) > 1:
+        # Combine the two nodes with the lowest frequencies
+        left = heapq.heappop(heap)
+        right = heapq.heappop(heap)
+        
+        # Create a new node with the combined frequency
+        combined_count = left.count + right.count
+        new_node = Node(combined_count, '\0',left,right)
+        
+        # Set the left and right children
+        new_node.left = left
+        new_node.right = right
+        
+        # Add the new node back to the heap
+        heapq.heappush(heap, new_node)
+    
+    # The last remaining node in the heap is the root of the Huffman tree
+    return heap[0]
+
+tree = make_tree(nodes)
+
+def add_edges(parent, G):
+    """Make a NetworkX graph that represents the tree."""
+    if parent is None:
+        return
+    
+    for child in (parent.left, parent.right):
+        if child:
+            G.add_edge(parent, child)
+            add_edges(child, G)
+
+# G = nx.DiGraph()
+# add_edges(tree, G)
+
+def get_labels(parent, labels):
+    if parent is None:
+        return
+    
+    if parent.letter == '\0':
+        labels[parent] = parent.count
+    else:
+        labels[parent] = parent.letter
+        
+    get_labels(parent.left, labels)
+    get_labels(parent.right, labels)
+
+# labels = {}
+# get_labels(tree, labels)
+
+def get_edge_labels(parent, edge_labels):
+    if parent is None:
+        return
+    
+    if parent.left:
+        edge_labels[parent, parent.left] = '0'
+        get_edge_labels(parent.left, edge_labels)
+        
+    if parent.right:
+        edge_labels[parent, parent.right] = '1'
+        get_edge_labels(parent.right, edge_labels)
+
+# edge_labels = {}
+# get_edge_labels(tree, edge_labels)
+# print(len(edge_labels))
+
+def draw_tree(tree):
+    G = nx.DiGraph()
+    add_edges(tree, G)
+    pos = hierarchy_pos(G)
+    labels = {}
+    get_labels(tree, labels)
+    edge_labels = {}
+    get_edge_labels(tree, edge_labels)
+    subax1 = plt.subplot()
+    nx.draw(G, pos, labels=labels, alpha=0.4)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='C1')
+    plt.show()
+
+draw_tree(tree)
